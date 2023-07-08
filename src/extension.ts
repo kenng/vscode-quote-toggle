7a005b3f5bccc61d302dde4676449fe0e9624617
Ken Ng <kenngsimply@gmail.com>
2023-07-08 11:37:26 +0800

    feat: able to toggle the single-double quote

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,71 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quote-toggle" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('quote-toggle.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('quote-toggle.toggleQuote', () => {
 		vscode.window.showInformationMessage('Hello World from Quote Toggle!');
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		const selections = editor.selections;
+		const document = editor.document;
+
+		editor.edit((editBuilder) => {
+			selections.forEach((selection) => {
+				const range = getRange(document, selection);
+				const selectedText = document.getText(range);
+				const toggledText = toggleQuotes(selectedText);
+
+				editBuilder.replace(range, toggledText);
+			});
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getRange(document: vscode.TextDocument, selection: vscode.Selection): vscode.Range {
+	const startChar = selection.start.character;
+	const endChar = selection.end.character;
+	const cursorLine = selection.active.line;
+	const lineText = document.lineAt(cursorLine).text;
+
+	let quoteStart = startChar;
+	let quoteEnd = endChar;
+
+	for (let i = startChar; i >= 0; i--) {
+		if (lineText[i] === "'" || lineText[i] === '"') {
+			quoteStart = i;
+			break;
+		}
+	}
+
+	for (let i = endChar; i < lineText.length; i++) {
+		if (lineText[i] === "'" || lineText[i] === '"') {
+			quoteEnd = i + 1;
+			break;
+		}
+	}
+
+	const range = new vscode.Range(
+		new vscode.Position(cursorLine, quoteStart),
+		new vscode.Position(cursorLine, quoteEnd));
+	return range;
+}
+
+function toggleQuotes(text: string) {
+	const singleQuoteRegex = /'/g;
+	const doubleQuoteRegex = /"/g;
+
+	if (singleQuoteRegex.test(text)) {
+		return text.replace(singleQuoteRegex, '"');
+	} else if (doubleQuoteRegex.test(text)) {
+		return text.replace(doubleQuoteRegex, "'");
+	} else {
+		return text;
+	}
+}
+
+export function deactivate() { }
